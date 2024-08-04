1ba621791814a2ae6a4b9f61eb31c237b64bd679
barnold8 <bandslover@outlook.com>
2024-08-04 17:15:19 +0100

    Add proper error management for mismanaged API key and faulty client handling

--- backend/Communicator.py
+++ backend/Communicator.py
@@ -1,20 +1,38 @@
 import os
 import asyncio
 from openai import AsyncOpenAI
-
-
+from typing import Type
 
 class Communicator:
-    _KEY = open("backend/API_KEY", "r").readline().rstrip()
-    _client = AsyncOpenAI(api_key=_KEY)
+
+    __KEY    = None 
+    __client = None
+
+
+
+
+    @classmethod
+    def initialise_client(cls: Type['Communicator']):
+
+        try:
+            cls.__KEY = open("backend/API_KEY", "r").readline().rstrip()
+            cls.__client = AsyncOpenAI(api_key=cls._KEY)
+        except (openai.AuthenticationError, FileNotFoundError) as e:
+            print(f"Error initializing OpenAI client: {e}")
 
     @staticmethod
     async def sendRequest(prompt: str, code: str) -> None:
 
+        if Communicator.__KEY == None:
+            print("No key") # return JSON response
+            return
+        elif Communicator.__client == None:
+            print("No client") # return JSON response
+            return
 
         message = f"{prompt}\n\nHere is the code:\n```{code}\n```"
 
-        stream = await Communicator._client.chat.completions.create(
+        stream = await Communicator.__client .chat.completions.create(
             model="gpt-4",
             messages=[{
                 "role": "user",
@@ -22,6 +40,7 @@ class Communicator:
                 }],
             stream=True,
         )
+
         async for chunk in stream:
             print(chunk.choices[0].delta.content or "", end="")
 
