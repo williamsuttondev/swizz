38f84e1d8e3f53dd9d973def708254a86dde569c
barnold8 <bandslover@outlook.com>
2024-08-04 16:59:02 +0100

    Add logic to Communicator class to allow prompts and code to be sent to GPT model 4

--- backend/Communicator.py
+++ backend/Communicator.py
@@ -1,9 +1,33 @@
-from openAI import openAI
+import os
+import asyncio
+from openai import AsyncOpenAI
+
+
 
 class Communicator:
+    _KEY = open("backend/API_KEY", "r").readline().rstrip()
+    _client = AsyncOpenAI(api_key=_KEY)
 
-    pass
+    @staticmethod
+    async def sendRequest(prompt: str, code: str) -> None:
+
+
+        message = f"{prompt}\n\nHere is the code:\n```{code}\n```"
+
+        stream = await Communicator._client.chat.completions.create(
+            model="gpt-4",
+            messages=[{
+                "role": "user",
+                 "content": message
+                }],
+            stream=True,
+        )
+        async for chunk in stream:
+            print(chunk.choices[0].delta.content or "", end="")
 
 
 
-print("Hello world!")
+message = "".join(open("frontend/main.py","r").readlines())
+
+
+asyncio.run(Communicator.sendRequest("Can you explain what this code does?", message))
