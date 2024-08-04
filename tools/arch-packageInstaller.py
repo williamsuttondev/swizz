6683917f3a817e8b73eb6a64afd9601b0ffce45b
barnold8 <bandslover@outlook.com>
2024-08-04 15:01:56 +0100

    Remove toolings, adding venv to work with pip directly

--- tools/arch-packageInstaller.py
+++
@@ -1,9 +0,0 @@
-# this file is so I can install needed packages system wide on my arch installation
-# run file as sudo to avoid needing to put password in at start
-
-import os
-
-with open("requirements.txt") as file:
-    
-    for line in file.readlines():
-        os.system(f"pacman -S python-{line}")
